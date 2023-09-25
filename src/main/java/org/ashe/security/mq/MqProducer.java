3d5de0defd9c571c6c8afdd41420842f6ec55ed9
A <ashet@qq.com>
2023-09-26 01:12:04 +0800

    chore: confirmCallback

--- src/main/java/org/ashe/security/mq/MqProducer.java
+++ src/main/java/org/ashe/security/mq/MqProducer.java
@@ -1,6 +1,7 @@
 package org.ashe.security.mq;
 
 import com.alibaba.fastjson2.JSON;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.ashe.security.config.MqConf;
@@ -13,6 +14,22 @@ import org.springframework.stereotype.Component;
 public class MqProducer {
 
     private final RabbitTemplate rabbitTemplate;
+
+    @PostConstruct
+    public void init() {
+        // Producer初始化为 confirm 模式，消息发送后会异步回调生产者
+        rabbitTemplate.setConfirmCallback((correlationData, ack, cause) -> {
+            if (ack) {
+                // 发送成功
+                log.info("----> Message sent successfully.");
+            } else {
+                // 发送失败
+                log.error("----> Failed to send message: " + cause);
+            }
+        });
+        rabbitTemplate.setMandatory(true);
+    }
+
 
     public void sendMessage(String routingKey, Object message) {
         rabbitTemplate.convertAndSend(MqConf.DIRECT_EXCHANGE, routingKey, JSON.toJSON(message));
